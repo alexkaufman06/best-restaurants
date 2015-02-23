7b350327c08b4d22f64690d58de6d5989b7e7a99
Alex Kaufman and Lizzie Koehler <lizziefkoehler@gmail.com>
2015-02-23 10:35:36 -0800

    Create delete and add functionality for restaurants

---
+++ js/RestaurantsController.js
@@ -0,0 +1,17 @@
+bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
+  $scope.restaurants = [];
+  $scope.addRestaurant = function() {
+    $scope.restaurants.push({ name: $scope.restaurantName,
+                              type: $scope.restaurantType,
+                              location: $scope.restaurantLocation,
+                              price: $scope.restaurantPrice });
+    $scope.restaurantName = null;
+    $scope.restaurantType = null;
+    $scope.restaurantLocation = null;
+    $scope.restaurantPrice = null;
+  };
+  $scope.deleteRestaurant = function(restaurant) {
+    var index = $scope.restaurants.indexOf(restaurant);
+    $scope.restaurants.splice(index, 1);
+  };
+});
