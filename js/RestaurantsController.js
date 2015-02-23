5db28d78a290ae357d55cf2cfa45202c20b9053d
Alex Kaufman and Lizzie Koehler <alexkaufman06@gmail.com>
2015-02-23 10:43:03 -0800

    capitalize the first letter of all input

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,10 +1,10 @@
 bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [];
   $scope.addRestaurant = function() {
-    $scope.restaurants.push({ name: $scope.restaurantName,
-                              type: $scope.restaurantType,
-                              location: $scope.restaurantLocation,
-                              price: $scope.restaurantPrice });
+    $scope.restaurants.push({ name: $scope.restaurantName.charAt(0).toUpperCase() + $scope.restaurantName.slice(1),
+                              type: $scope.restaurantType.charAt(0).toUpperCase() + $scope.restaurantType.slice(1),
+                              location: $scope.restaurantLocation.charAt(0).toUpperCase() + $scope.restaurantLocation.slice(1),
+                              price: $scope.restaurantPrice.charAt(0).toUpperCase() + $scope.restaurantPrice.slice(1) });
     $scope.restaurantName = null;
     $scope.restaurantType = null;
     $scope.restaurantLocation = null;
